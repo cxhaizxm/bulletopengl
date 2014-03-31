a443f489c1b40a8145782a72e7ac46c9d8d77300
consultit <consultit@katamail.com>
2014-03-31 12:47:15 +0200

    * Added Ch2.1 files.

--- src/ch1/main.cpp
+++
@@ -1,8 +0,0 @@
-/*ADD*/	#include "BulletOpenGLApplication.h"
-/*ADD*/	#include "FreeGLUTCallbacks.h"
-/*ADD*/	
-int main(int argc, char** argv)
-{
-/*ADD*/		BulletOpenGLApplication demo;
-/*ADD*/		return glutmain(argc, argv, 1024, 768, "Introduction to Game Physics with Bullet Physics and OpenGL", &demo);
-}
